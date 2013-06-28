03d3dd46f8e1bc01069a9c67b9b96dd8cbee4671
Jan Antala <jan@antala.sk>
2013-06-28 10:09:27 +0200

    app seed

---
+++ github-profile.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+/**
+ * HTTP SERVER AND ROUTES
+ */
+
+var routes = require('./routes');
+var uploads = require('./routes/uploads');
+var analytics = require('./routes/analytics');
+
+app.get('/', routes.index);
+
+app.get('/analytics/:user/:csv', analytics.analyze);
+app.post('/upload', uploads.csv);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+/**
+ * CRON JOB
+ */
+
+var clenup = require('cron').CronJob;
+new clenup('0 0 */1 * * *', function(){
+	uploads.clear();
+}, null, true, "Europe/Bratislava");
---
+++ routes/analytics.js
@@ -0,0 +1,8 @@
+
+/*
+ * Analytics.
+ */
+
+exports.analyze = function(req, res){
+  res.send(200);
+};
