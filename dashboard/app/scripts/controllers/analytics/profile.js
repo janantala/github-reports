fb769931aa9cf0e1593e491d6e2cb2893f10e02f
Jan Antala <jan@antala.sk>
2013-07-01 00:20:55 +0200

    frontend reports

--- dashboard/app/scripts/controllers/analytics/profile.js
+++ dashboard/app/scripts/controllers/analytics/profile.js
@@ -4,4 +4,14 @@ angular.module('dashboardApp')
 .controller('AnalyticsProfileCtrl', ['$scope', 'analytics', '$routeParams', function ($scope, analytics, $routeParams) {
   $scope.analytics = analytics;
   $scope.routeParams = $routeParams;
+
+  $scope.events = ['PushEvent', 'PullRequestEvent', 'IssuesEvent'];
+
+  $scope.getTotal = function(item, events){
+    var sum = 0;
+    events.forEach(function(event){
+      sum += item[event];
+    });
+    return sum;
+  };
 }]);
