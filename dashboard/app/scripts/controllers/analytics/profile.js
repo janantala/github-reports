64ac69a834dcddfb6004426766e2b9ad4a9c5303
Jan Antala <jan@antala.sk>
2013-07-01 14:21:26 +0200

    strings

--- dashboard/app/scripts/controllers/analytics/profile.js
+++ dashboard/app/scripts/controllers/analytics/profile.js
@@ -7,6 +7,54 @@ angular.module('dashboardApp')
 
   $scope.events = ['PushEvent', 'PullRequestEvent', 'IssuesEvent'];
   $scope.dayHours = [];
+  $scope.strings = {
+    "DAY": {
+      "0": "Sunday",
+      "1": "Monday",
+      "2": "Tuesday",
+      "3": "Wednesday",
+      "4": "Thursday",
+      "5": "Friday",
+      "6": "Saturday"
+    },
+    "MONTH": {
+      "0": "January",
+      "1": "February",
+      "2": "March",
+      "3": "April",
+      "4": "May",
+      "5": "June",
+      "6": "July",
+      "7": "August",
+      "8": "September",
+      "9": "October",
+      "10": "November",
+      "11": "December"
+    },
+    "SHORTDAY": {
+      "0": "Sun",
+      "1": "Mon",
+      "2": "Tue",
+      "3": "Wed",
+      "4": "Thu",
+      "5": "Fri",
+      "6": "Sat"
+    },
+    "SHORTMONTH": {
+      "0": "Jan",
+      "1": "Feb",
+      "2": "Mar",
+      "3": "Apr",
+      "4": "May",
+      "5": "Jun",
+      "6": "Jul",
+      "7": "Aug",
+      "8": "Sep",
+      "9": "Oct",
+      "10": "Nov",
+      "11": "Dec"
+    }
+  };
 
   for (var h=0; h<24;h++){
     $scope.dayHours.push(h);
