56103eccf5b62bb36b7a48145a873b707ce67e22
Jan Antala <jan@antala.sk>
2013-07-01 12:26:00 +0200

    weekhours - get max contributions

--- dashboard/app/scripts/controllers/analytics/profile.js
+++ dashboard/app/scripts/controllers/analytics/profile.js
@@ -14,4 +14,18 @@ angular.module('dashboardApp')
     });
     return sum;
   };
+
+  var getMax = function(week, events){
+    var max = 0;
+    week.forEach(function(day){
+      day.forEach(function(hour){
+        if (max < $scope.getTotal(hour, events)){
+          max = $scope.getTotal(hour, events);
+        }
+      });
+    });
+    return max;
+  };
+
+  $scope.max = getMax($scope.analytics.weekhoursArr, $scope.events);
 }]);
