7fdeb353ee4a6ac45128b953ac691b6ff6e6514e
Jan Antala <jan@antala.sk>
2013-07-07 01:41:27 +0200

    angular update version

--- dashboard/app/scripts/services/Analytics.js
+++ dashboard/app/scripts/services/Analytics.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('dashboardApp')
-.factory('Analytics', ['$resource', function() {
+.factory('Analytics', [function() {
 
   return {
     'sse': function(csv, timezone, ondata){
