833016697508211c98cd10487347afafef583eae
Jan Antala <jan@antala.sk>
2013-07-07 01:07:19 +0200

    loading data alert

--- dashboard/app/scripts/services/Analytics.js
+++ dashboard/app/scripts/services/Analytics.js
@@ -7,15 +7,21 @@ angular.module('dashboardApp')
     'sse': function(csv, timezone, ondata){
       console.log('sse init');
       var source = new EventSource('/analytics/?csv=' + csv + '&timezone=' + timezone);
-
+      var that = this;
       source.addEventListener('message', function(e) {
         var data = JSON.parse(e.data);
         ondata(data);
       }, false);
 
       source.addEventListener('error', function(e) {
-       source.close();
+        source.close();
+        if (that.loaded){
+          that.loaded();
+        }
       }, false);
+    },
+    'loaded': function(loaded){
+      this.loaded = loaded;
     }
   };
 }]);
