ff7a8de90216872b43ef28de19e55ccbc8cc109a
Jan Antala <jan@antala.sk>
2013-07-07 00:51:31 +0200

    remove tmp csv

--- routes/uploads.js
+++ routes/uploads.js
@@ -16,5 +16,39 @@ exports.csv = function(req, res){
 
 
 exports.clear = function(req, res){
+  var dir = process.cwd() + '/uploads/';
+  var fs = require('fs');
+  fs.readdir(dir, function(err, files){
+    if (err) {
+      console.log(err);
+      return false;
+    }
 
+    var csvs = [];
+    files.forEach(function(file){
+      if (file !== '.gitignore') {
+        csvs.push(file);
+      }
+    });
+
+    var MAXAGE = 1000 * 60 * 60; // 1 hr
+    var time = new Date().getTime();
+    var remove = [];
+    csvs.forEach(function(csv){
+      var age = time - fs.statSync(dir + csv).mtime.getTime();
+      if (age > MAXAGE) {
+        remove.push(csv);
+      }
+    });
+
+    remove.forEach(function(csv){
+      console.log('Removing tmp csv', dir + csv);
+      fs.unlink(dir + csv, function (err) {
+        if (err) {
+          console.log(err);
+          return false;
+        }
+      });
+    });
+  });
 };
